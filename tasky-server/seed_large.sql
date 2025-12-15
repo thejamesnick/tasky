aa781089dc9e1d6769731ee4b0aff074105cc7ca
Nicholas James <thejamesnick@gmail.com>
2025-12-15 16:36:26 +0100

    feat: Install or update project dependencies.

--- tasky-server/seed_large.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO sheets (title, content, color, target_date, group_id) VALUES 
-('Design System', '<ul data-type="taskList"><li data-type="taskItem" data-checked="true">Typography</li><li data-type="taskItem" data-checked="true">Colors</li></ul>', '#93c5fd', '2025-11-30', 'demo_group_1'),
-('Design System', '<ul data-type="taskList"><li data-type="taskItem" data-checked="true">Buttons</li><li data-type="taskItem" data-checked="false">Inputs</li></ul>', '#93c5fd', '2025-11-29', 'demo_group_1'),
-('Design System', '<ul data-type="taskList"><li data-type="taskItem" data-checked="true">Modals</li><li data-type="taskItem" data-checked="true">Toasts</li></ul>', '#93c5fd', '2025-11-28', 'demo_group_1'),
-('Design System', '<ul data-type="taskList"><li data-type="taskItem" data-checked="false">Grid Layout</li></ul>', '#93c5fd', '2025-11-27', 'demo_group_1'),
-('Design System', '<ul data-type="taskList"><li data-type="taskItem" data-checked="true">Icons</li></ul>', '#93c5fd', '2025-11-26', 'demo_group_1'),
-('Design System', '<ul data-type="taskList"><li data-type="taskItem" data-checked="true">Spacing</li></ul>', '#93c5fd', '2025-11-25', 'demo_group_1'),
-('Design System', '<ul data-type="taskList"><li data-type="taskItem" data-checked="true">Shadows</li></ul>', '#93c5fd', '2025-11-24', 'demo_group_1');
